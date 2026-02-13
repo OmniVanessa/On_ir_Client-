7f648c603c45629fd5b6288345f5164e44b9eafa
OmniVanessa <vanessa.remicio@omni.pro>
2026-02-13 16:57:17 -0500

    Actualizacion para dar con link del servidor

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // permite conexiones externas
+    host: true,
     allowedHosts: [
       'fleshier-signatureless-gustavo.ngrok-free.dev'
-    ]
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
